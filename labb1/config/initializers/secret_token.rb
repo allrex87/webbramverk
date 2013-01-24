6548e34a7a5484b33e9ebf64b2d9ace9bb7a9ed2
Alexandra Johansson <aj222im@stundent.lnu.se>
2013-01-24 18:02:47 +0100

    labb 1 new project

---
+++ labb1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kod::Application.config.secret_token = '7829d11482f7f7cc2ce1efe2a6135fdc119c83b3afb355b46bf7c73fea9a6ba461afc228636662347645ae80722f255bba6d7f554da12114a39ad83a031f3aa5'
